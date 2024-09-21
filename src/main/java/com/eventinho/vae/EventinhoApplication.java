ea5b3aa093ccb44899d12250c6151dfc0ee08e1a
anacmenezes <anamenezes.dev@gmail.com>
2024-09-21 17:02:02 -0300

    Projeto criado

---
+++ src/main/java/com/eventinho/vae/EventinhoApplication.java
@@ -0,0 +1,13 @@
+package com.eventinho.vae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventinhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventinhoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eventinho/vae/EventinhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventinho.vae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventinhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
